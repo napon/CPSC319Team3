901cc0e7341df474e34b33419f40b9bd5eba7e6a
Noah <Noah.sommerfeld@gmail.com>
2016-02-03 12:26:11 -0800

    Made a test comment

--- PluriLockItup/app/src/main/java/cpsc319/team3/com/plurilockitup/MainActivity.java
+++ PluriLockItup/app/src/main/java/cpsc319/team3/com/plurilockitup/MainActivity.java
@@ -7,6 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Noah's test commit
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
