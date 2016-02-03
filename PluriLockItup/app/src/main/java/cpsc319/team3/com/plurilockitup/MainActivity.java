89cf8f0744d513002f880e99b4de0a71151669c7
KChan <chan.kelv@gmail.com>
2016-02-03 11:57:35 -0800

    Init commit

---
+++ PluriLockItup/app/src/main/java/cpsc319/team3/com/plurilockitup/MainActivity.java
@@ -0,0 +1,13 @@
+package cpsc319.team3.com.plurilockitup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
