8e0f9f171822c59ffb1cf82091dceca552d3f86d
KChan <chan.kelv@gmail.com>
2016-02-05 01:01:52 -0800

    list view row adaptor made

--- PluriLockItup/app/src/main/java/cpsc319/team3/com/plurilockitup/activity/MainActivity.java
+++ PluriLockItup/app/src/main/java/cpsc319/team3/com/plurilockitup/activity/MainActivity.java
@@ -1,16 +1,23 @@
 package cpsc319.team3.com.plurilockitup.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
 
 import cpsc319.team3.com.plurilockitup.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //Noah's test commit
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listview = (ListView) findViewById(R.id.dayAcctTable);
+        listview.setAdapter(new yourAdapter(this, new String[]{"data1",
+                "data2"}));
     }
 }
---
+++ PluriLockItup/app/src/main/java/cpsc319/team3/com/plurilockitup/activity/yourAdapter.java
@@ -0,0 +1,56 @@
+package cpsc319.team3.com.plurilockitup.activity;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import cpsc319.team3.com.plurilockitup.R;
+
+/**
+ * Created by kelvinchan on 16-02-05.
+ */
+class yourAdapter extends BaseAdapter {
+
+    Context context;
+    String[] data;
+    private static LayoutInflater inflater = null;
+
+    public yourAdapter(Context context, String[] data) {
+        // TODO Auto-generated constructor stub
+        this.context = context;
+        this.data = data;
+        inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        // TODO Auto-generated method stub
+        return data.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        // TODO Auto-generated method stub
+        return data[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        // TODO Auto-generated method stub
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // TODO Auto-generated method stub
+        View vi = convertView;
+        if (vi == null)
+            vi = inflater.inflate(R.layout.acct_row, null);
+//        TextView text = (TextView) vi.findViewById(R.id.text);
+//        text.setText(data[position]);
+        return vi;
+    }
+}
