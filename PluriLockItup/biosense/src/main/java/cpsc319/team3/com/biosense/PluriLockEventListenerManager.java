4248c6fce372794662b9a4fe8593f87d7ff0ee95
leesunny <dev@leesunny.com>
2016-02-14 16:08:27 -0800

    Added skeleton eventListenerManager class

---
+++ PluriLockItup/biosense/src/main/java/cpsc319/team3/com/biosense/PluriLockEventListenerManager.java
@@ -0,0 +1,25 @@
+package cpsc319.team3.com.biosense;
+
+import android.view.View;
+
+/**
+ * Created by Sunny on 2016-02-14.
+ */
+public class PluriLockEventListenerManager {
+
+    public View.OnClickListener createClickListener() {
+        throw new RuntimeException("Not yet implemented");
+    }
+
+    public View.OnLongClickListener createLongClickListener() {
+        throw new RuntimeException("Not yet implemented");
+    }
+
+    public View.OnKeyListener createKeyListener() {
+        throw new RuntimeException("Not yet implemented");
+    }
+
+    public View.OnTouchListener createTouchListener() {
+        throw new RuntimeException("Not yet implemented");
+    }
+}
