c152a3e4e5f397f22bfa2bec3c7b4eddc1027b50
KChan <chan.kelv@gmail.com>
2016-02-16 13:31:33 -0700

    very basic map created. Added internet and network status permissions. Added maps API key

---
+++ PluriLockItup/app/src/main/java/cpsc319/team3/com/plurilockitup/activity/MapLocationActivity.java
@@ -0,0 +1,33 @@
+package cpsc319.team3.com.plurilockitup.activity;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import cpsc319.team3.com.plurilockitup.R;
+
+public class MapLocationActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_location);
+
+        //Generate map
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        map.addMarker(new MarkerOptions()
+                .position(new LatLng(0, 0))
+                .title("Marker"));
+    }
+}
