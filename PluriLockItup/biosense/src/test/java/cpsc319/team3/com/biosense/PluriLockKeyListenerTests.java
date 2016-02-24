3f173c68335463bee2f6d6a9280adbdcdc5d3605
Karen Guo <karenguo@live.com>
2016-02-23 22:15:04 -0800

    added sample tests into listeners

--- PluriLockItup/biosense/src/test/java/cpsc319/team3/com/biosense/PluriLockKeyListenerTests.java
+++ PluriLockItup/biosense/src/test/java/cpsc319/team3/com/biosense/PluriLockKeyListenerTests.java
@@ -1,6 +1,13 @@
 package cpsc319.team3.com.biosense;
 
 import org.junit.Test;
+import static org.junit.Assert.assertTrue;
 
 public class PluriLockKeyListenerTests {
+    @Test
+    public void sampleTest() throws Exception {
+        PluriLockEventTracker pluriLockEventTracker = new PluriLockEventTracker();
+        PluriLockKeyListener p = new PluriLockKeyListener(pluriLockEventTracker);
+        assertTrue(p.getClass() == PluriLockKeyListener.class);
+    }
 }
--- PluriLockItup/biosense/src/test/java/cpsc319/team3/com/biosense/PluriLockTouchListenerTests.java
+++ PluriLockItup/biosense/src/test/java/cpsc319/team3/com/biosense/PluriLockTouchListenerTests.java
@@ -1,6 +1,13 @@
 package cpsc319.team3.com.biosense;
 
 import org.junit.Test;
+import static org.junit.Assert.assertTrue;
 
 public class PluriLockTouchListenerTests {
+    @Test
+    public void sampleTest() throws Exception {
+        PluriLockEventTracker pluriLockEventTracker = new PluriLockEventTracker();
+        PluriLockTouchListener p = new PluriLockTouchListener(pluriLockEventTracker);
+        assertTrue(p.getClass() == PluriLockTouchListener.class);
+    }
 }
