ea5fafd418b58670a6a502c8e5232de431e16843
Noah <Noah.sommerfeld@gmail.com>
2016-02-24 12:28:36 -0800

    Changed PluriLockAPI to be a (modified) Singleton

--- PluriLockItup/biosense/src/main/java/cpsc319/team3/com/biosense/PluriLockAPI.java
+++ PluriLockItup/biosense/src/main/java/cpsc319/team3/com/biosense/PluriLockAPI.java
@@ -12,7 +12,19 @@ import cpsc319.team3.com.biosense.exception.LocationServiceUnavailableException;
  */
 public class PluriLockAPI {
     private PluriLockEventManager eventManager;
+    private static PluriLockAPI mySession;
+    
     //TODO: add listeners
+    public static PluriLockAPI getInstance(){
+        return mySession;
+    }
+
+    public static PluriLockAPI createNewSession(Context context, PluriLockServerResponseListener callback,
+                                   String userID, PluriLockConfig config)
+                                    throws LocationServiceUnavailableException{
+        mySession = new PluriLockAPI(context,callback,userID,config);
+        return mySession;
+    }
 
     public PluriLockAPI(Context context, PluriLockServerResponseListener callback, String id,
                         PluriLockConfig config) throws LocationServiceUnavailableException {
--- PluriLockItup/biosense/src/test/java/cpsc319/team3/com/biosense/PluriLockAPITests.java
+++ PluriLockItup/biosense/src/test/java/cpsc319/team3/com/biosense/PluriLockAPITests.java
@@ -1,13 +1,60 @@
+
 package cpsc319.team3.com.biosense;
 
+import android.content.Context;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
+
+import cpsc319.team3.com.biosense.exception.LocationServiceUnavailableException;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
+@RunWith(RobolectricTestRunner.class)
 public class PluriLockAPITests {
-//    @Test
-//    public void sampleTest() throws Exception {
-//        PluriLockAPI p = new PluriLockAPI();
-//        assertTrue(p.getClass() == PluriLockAPI.class);
-//    }
+
+    private PluriLockServerResponseListener baseListener;
+    private PluriLockConfig baseConfig;
+    Context testContext;
+    @Before
+    public void setup() {
+        testContext = RuntimeEnvironment.application.getApplicationContext();
+        baseConfig = new PluriLockConfig();
+        baseListener = new PluriLockServerResponseListener() {
+            @Override
+            public void notify(String msg) {
+                //do nothing.
+            }
+        };
+    }
+
+//===============BASIC BUILD TESTS=====================
+   @Test
+    public void noBuiltAPIShouldBeNull() throws Exception {
+        assertTrue(PluriLockAPI.getInstance() == null);
+    }
+
+    @Test
+    public void standardBuildTestShouldNotThrowErrors(){
+
+        try{
+            PluriLockAPI.createNewSession(this.testContext,this.baseListener,"TEST",this.baseConfig);
+        }
+        catch(Exception e){
+            fail();
+        }
+    }
+
+    @Test
+    public void afterBuildShouldNotBeNull() throws LocationServiceUnavailableException {
+        assertTrue(PluriLockAPI.getInstance() == null);
+        PluriLockAPI.createNewSession(this.testContext, this.baseListener, "TEST", this.baseConfig);
+        assertTrue(PluriLockAPI.getInstance() != null);
+    }
+//===============END OF BASIC BUILD TESTS=====================
+
 }
