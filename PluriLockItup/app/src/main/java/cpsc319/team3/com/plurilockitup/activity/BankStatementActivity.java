08fe235e13ef783b0fa4aab880adde0fe30f4737
KChan <chan.kelv@gmail.com>
2016-02-29 14:52:45 -0800

    Added purchase lines for statements added fail logout condition added indication to swipe in statements

--- PluriLockItup/app/src/main/java/cpsc319/team3/com/plurilockitup/activity/BankStatementActivity.java
+++ PluriLockItup/app/src/main/java/cpsc319/team3/com/plurilockitup/activity/BankStatementActivity.java
@@ -6,6 +6,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import cpsc319.team3.com.plurilockitup.R;
 
@@ -13,6 +14,8 @@ public class BankStatementActivity extends AppCompatActivity {
 
     FragmentPagerAdapter fmPageAdapter;
     ViewPager mainPager;
+    boolean swipeHelper = true;
+    String [] monthList = new String[]{"January", "February", "March", "April"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +25,17 @@ public class BankStatementActivity extends AppCompatActivity {
         fmPageAdapter = new MainPagerAdapter(getSupportFragmentManager());
         mainPager = (ViewPager)findViewById(R.id.viewpager);
         mainPager.setAdapter(fmPageAdapter);
+
+
+        mainPager.addOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener(){
+            @Override
+            public void onPageSelected(int position){
+                if(swipeHelper){
+                    findViewById(R.id.swipeReminder).setVisibility(View.GONE);
+                    swipeHelper = false;
+                }
+            }
+        });
     }
 
 
@@ -51,7 +65,12 @@ public class BankStatementActivity extends AppCompatActivity {
 //                default:
 //                    return null;
 //            }
-            return new StatementFragment();
+            Fragment statementFrag = new StatementFragment();
+            Bundle bundle = new Bundle();
+            bundle.putString("month", monthList[position]);
+            bundle.putStringArray("monthList", monthList);
+            statementFrag.setArguments(bundle);
+            return statementFrag;
         }
 
 
--- PluriLockItup/app/src/main/java/cpsc319/team3/com/plurilockitup/activity/StatementFragment.java
+++ PluriLockItup/app/src/main/java/cpsc319/team3/com/plurilockitup/activity/StatementFragment.java
@@ -6,20 +6,37 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TableLayout;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import cpsc319.team3.com.plurilockitup.R;
+import cpsc319.team3.com.plurilockitup.model.CreditStatement;
 
 public class StatementFragment extends Fragment {
+    String month;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_statement, container, false);
-
         TableLayout statementTable = (TableLayout) view.findViewById(R.id.statementTable);
 
-        for(int i = 0;i<5;i++){
+        this.month = getArguments().getString("month");
+        ((TextView)view.findViewById(R.id.purchase_month)).setText(month);
+        String[] monthList = getArguments().getStringArray("monthList");
+
+        for(int i = 0;i<monthList.length;i++){
             View row = getLayoutInflater(savedInstanceState).inflate(R.layout.statement_row, null);
+
+            CreditStatement creditStatement = new CreditStatement(5);
+            HashMap<String, String> purchaseList = creditStatement.getPurchaseList();
+            for(Map.Entry<String, String> purchase: purchaseList.entrySet()){
+                ((TextView) row.findViewById(R.id.purchase_name)).setText(purchase.getKey());
+                ((TextView) row.findViewById(R.id.purchase_amount)).setText(purchase.getValue());
+            }
+
             statementTable.addView(row);
         }
 
