6a24af4cdfec6c2f4770726fe91e1b5006f44402
leesunny <dev@leesunny.com>
2016-02-29 10:54:17 -0800

    Added mock objects for failing listener tests

--- PluriLockItup/biosense/src/test/java/cpsc319/team3/com/biosense/PluriLockTouchListenerTests.java
+++ PluriLockItup/biosense/src/test/java/cpsc319/team3/com/biosense/PluriLockTouchListenerTests.java
@@ -1,15 +1,20 @@
 package cpsc319.team3.com.biosense;
 
+import android.content.Context;
 import android.view.MotionEvent;
 import android.view.View;
 
 import org.junit.Test;
+import org.mockito.Mockito;
+
 import static org.junit.Assert.assertTrue;
 
 public class PluriLockTouchListenerTests {
     @Test
     public void constructorTest() {
-        PluriLockEventTracker pluriLockEventTracker = new PluriLockEventTracker(null, null);
+        PluriLockEventTracker pluriLockEventTracker = new PluriLockEventTracker(
+                Mockito.mock(Context.class),
+                Mockito.mock(PluriLockEventManager.class));
         PluriLockTouchListener p = new PluriLockTouchListener(pluriLockEventTracker);
         assertTrue(p.getClass() == PluriLockTouchListener.class);
     }
@@ -17,7 +22,9 @@ public class PluriLockTouchListenerTests {
     @Test
     public void onTouchReturnTrueTest() {
         View v = new View(null);
-        PluriLockEventTracker pluriLockEventTracker = new PluriLockEventTracker(null, null);
+        PluriLockEventTracker pluriLockEventTracker = new PluriLockEventTracker(
+                Mockito.mock(Context.class),
+                Mockito.mock(PluriLockEventManager.class));
         PluriLockTouchListener touchListener = new PluriLockTouchListener(pluriLockEventTracker);
 
         assertTrue(touchListener.onDoubleTap(null));
