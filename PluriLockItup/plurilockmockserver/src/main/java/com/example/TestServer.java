f9e3d068b333cf477f8b4d3195fe33e06d4b5b79
Elaine Feng <eleanor52@live.ca>
2016-02-28 22:56:05 -0800

    Fixing call to server in PluriLockNetworkUtil

--- PluriLockItup/plurilockmockserver/src/main/java/com/example/TestServer.java
+++ PluriLockItup/plurilockmockserver/src/main/java/com/example/TestServer.java
@@ -31,13 +31,13 @@ public class TestServer {
     public void onMessage(String message, Session session) throws IOException,
             InterruptedException {
         System.out.println("User input: " + message);
-        session.getBasicRemote().sendText("Hello world Mr. " + message);
+        session.getBasicRemote().sendText("Hello world " + message);
         // Sending message to client each 5 second
-        for (int i = 0; i <= 25; i++) {
-            session.getBasicRemote().sendText(i + " Message from server");
-            Thread.sleep(5000);
-
-        }
+////        for (int i = 0; i <= 25; i++) {
+////            session.getBasicRemote().sendText(i + " Message from server");
+////            Thread.sleep(5000);
+//
+//        }
     }
 
     @OnOpen
