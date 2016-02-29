869d09f15f445c58efec1a3d03fdf858167edfc0
KChan <chan.kelv@gmail.com>
2016-02-29 12:33:50 -0800

    pull from master, hooked up button listener in main d2d acct, made credit statements swappable fragment

--- PluriLockItup/app/src/main/java/cpsc319/team3/com/plurilockitup/activity/StatementFragment.java
+++ PluriLockItup/app/src/main/java/cpsc319/team3/com/plurilockitup/activity/StatementFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TableLayout;
 
 import cpsc319.team3.com.plurilockitup.R;
 
@@ -14,6 +15,13 @@ public class StatementFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_statement, container, false);
+
+        TableLayout statementTable = (TableLayout) view.findViewById(R.id.statementTable);
+
+        for(int i = 0;i<5;i++){
+            View row = getLayoutInflater(savedInstanceState).inflate(R.layout.statement_row, null);
+            statementTable.addView(row);
+        }
 
         return view;
     }
---
+++ PluriLockItup/app/src/main/java/cpsc319/team3/com/plurilockitup/model/CreditStatement.java
@@ -0,0 +1,8 @@
+package cpsc319.team3.com.plurilockitup.model;
+
+/**
+ * Created by kelvinchan on 16-02-29.
+ */
+public class CreditStatement {
+
+}
