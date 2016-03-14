5ff41355de14a3c2f7605a4aeb5d36576b922f78
Karen Guo <karenguo@live.com>
2016-03-13 21:46:18 -0700

    implement scale in listener

--- PluriLockItup/biosense/src/main/java/cpsc319/team3/com/biosense/PluriLockTouchListener.java
+++ PluriLockItup/biosense/src/main/java/cpsc319/team3/com/biosense/PluriLockTouchListener.java
@@ -4,6 +4,7 @@ import android.graphics.PointF;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.view.View;
 
 import java.util.GregorianCalendar;
@@ -20,7 +21,8 @@ import cpsc319.team3.com.biosense.models.PScrollEvent;
  *
  */
 public class PluriLockTouchListener implements
-        GestureDetector.OnGestureListener, View.OnTouchListener
+        GestureDetector.OnGestureListener, View.OnTouchListener,
+        ScaleGestureDetector.OnScaleGestureListener
 {
     private static final String TAG = "PluriLockTouchListener";
 
@@ -221,5 +223,56 @@ public class PluriLockTouchListener implements
     public boolean onTouch(View v, MotionEvent event) {
 
         return true;
+    }
+
+    /**
+     * Responds to scaling events for a gesture in progress.
+     * Reported by pointer motion.
+     *
+     * @param detector The detector reporting the event - use this to
+     *                 retrieve extended info about event state.
+     * @return Whether or not the detector should consider this event
+     * as handled. If an event was not handled, the detector
+     * will continue to accumulate movement until an event is
+     * handled. This can be useful if an application, for example,
+     * only wants to update scaling factors if the change is
+     * greater than 0.01.
+     */
+    @Override
+    public boolean onScale(ScaleGestureDetector detector) {
+        return false;
+    }
+
+    /**
+     * Responds to the beginning of a scaling gesture. Reported by
+     * new pointers going down.
+     *
+     * @param detector The detector reporting the event - use this to
+     *                 retrieve extended info about event state.
+     * @return Whether or not the detector should continue recognizing
+     * this gesture. For example, if a gesture is beginning
+     * with a focal point outside of a region where it makes
+     * sense, onScaleBegin() may return false to ignore the
+     * rest of the gesture.
+     */
+    @Override
+    public boolean onScaleBegin(ScaleGestureDetector detector) {
+        return false;
+    }
+
+    /**
+     * Responds to the end of a scale gesture. Reported by existing
+     * pointers going up.
+     * <p/>
+     * Once a scale has ended, {@link ScaleGestureDetector#getFocusX()}
+     * and {@link ScaleGestureDetector#getFocusY()} will return focal point
+     * of the pointers remaining on the screen.
+     *
+     * @param detector The detector reporting the event - use this to
+     *                 retrieve extended info about event state.
+     */
+    @Override
+    public void onScaleEnd(ScaleGestureDetector detector) {
+
     }
 }
