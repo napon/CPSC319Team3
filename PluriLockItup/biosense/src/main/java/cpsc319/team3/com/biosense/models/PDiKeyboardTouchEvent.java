5855d90197cbafddd5c2e0281d6bf271339affcd
KChan <chan.kelv@gmail.com>
2016-03-22 18:14:38 -0700

    Added digraphs. if key strokes under 1s will count as digraph

--- PluriLockItup/biosense/src/main/java/cpsc319/team3/com/biosense/models/PDiKeyboardTouchEvent.java
+++ PluriLockItup/biosense/src/main/java/cpsc319/team3/com/biosense/models/PDiKeyboardTouchEvent.java
@@ -25,8 +25,8 @@ public class PDiKeyboardTouchEvent extends PluriLockEvent {
     public PDiKeyboardTouchEvent(int screenOrientation, long timestamp,
                                  long duration, String fromKey, String toKey) {
         super(screenOrientation, timestamp, duration);
-        this.fromKey = "KEYCODE_" + fromKey.toUpperCase();
-        this.toKey = "KEYCODE_" + toKey.toUpperCase();
+        this.fromKey = (fromKey.startsWith("KEYCODE")) ?  fromKey : "KEYCODE_" + fromKey.toUpperCase();
+        this.toKey = (toKey.startsWith("KEYCODE")) ? toKey : "KEYCODE_" + toKey.toUpperCase();
     }
 
     /**
